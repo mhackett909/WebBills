2d26d701dac9ac6396a9834e73db3d3a31c06f10
mhackett909 <mhackett909@gmail.com>
2025-05-10 01:19:09 -0700

    query fix

--- src/main/java/com/projects/bills/Repositories/EntryRepository.java
+++ src/main/java/com/projects/bills/Repositories/EntryRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EntryRepository extends JpaRepository<Entry, Integer> {
-	@Query(value = "select * from entry e natural join bill b " +
-			"where e.date >= DATE_SUB(NOW(), INTERVAL 90 DAY) and b.status=1 " +
+	@Query(value = "select e.* " +
+			"from entry e " +
+			"join bill b on e.name = b.name " +
+			"where e.date >= DATE_SUB(NOW(), INTERVAL 90 DAY) and b.status = 1 " +
 			"order by e.date desc", nativeQuery = true)
 	List<Entry> findEntryLast90Days();
-	
 }
