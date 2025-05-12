1128e7927fdb85c83594f220d20edaa58d948f16
Michael Hackett <mhackett909@gmail.com>
2025-05-11 21:53:09 -0700

    Update BillDTO.java

--- src/main/java/com/projects/bills/DTOs/BillDTO.java
+++ src/main/java/com/projects/bills/DTOs/BillDTO.java
@@ -1,15 +1,15 @@
 package com.projects.bills.DTOs;
 
 public class BillDTO {
-		private long id;
-		private String name;
-		private Boolean status; //isArchived
+	private long id;
+	private String name;
+	private Boolean status; //isArchived
 
-		public String getName() { return name; }
-	    public Boolean getStatus() { return status; }
-	    public long getId() { return id; }
+	public String getName() { return name; }
+	public Boolean getStatus() { return status; }
+	public long getId() { return id; }
 
-	    public void setName(String name) { this.name = name; }
-		public void setStatus(Boolean status) { this.status = status; }
-	    public void setId(long id) { this.id = id; }
+	public void setName(String name) { this.name = name; }
+	public void setStatus(Boolean status) { this.status = status; }
+	public void setId(long id) { this.id = id; }
 }
