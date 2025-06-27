63fbd294871d031187d229bddbe1dc8b6cd2bd6d
mhackett909 <mhackett909@gmail.com>
2025-06-27 14:19:26 -0700

    primitive long for entities

--- src/main/java/com/projects/bills/Entities/User.java
+++ src/main/java/com/projects/bills/Entities/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(nullable = false, unique = true)
     private String username;
